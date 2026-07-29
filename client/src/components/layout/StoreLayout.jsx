173d60afa3e8d94399e037eec71e53ee5f6bf7ca
shyam-08-02-2004/form <shyamdangi084@gmail.com>
2026-07-29 22:13:50 +0530

    style: Render footer only on home page

--- client/src/components/layout/StoreLayout.jsx
+++ client/src/components/layout/StoreLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import CategoryNav from './CategoryNav';
 import Footer from './Footer';
@@ -6,6 +6,8 @@ import MobileBottomNav from './MobileBottomNav';
 import GlobalPopup from '../ui/GlobalPopup';
 
 const StoreLayout = () => {
+  const location = useLocation();
+
   return (
     <div className="flex flex-col min-h-screen bg-[#f1f3f6] font-sans pb-14 md:pb-0">
       <Navbar />
@@ -13,7 +15,7 @@ const StoreLayout = () => {
       <main className="flex-grow">
         <Outlet />
       </main>
-      <Footer />
+      {location.pathname === '/' && <Footer />}
       <MobileBottomNav />
       <GlobalPopup />
     </div>
